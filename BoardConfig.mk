45d2a19dcdb194a3244fd06b0f58406fc4f0926a
The-DarkBeast <pranay@firehound.org>
2018-07-12 13:49:29 +0530

    garlic: Use clang to compile the kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 -
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CONFIG := lineage_garlic_defconfig
 TARGET_KERNEL_SOURCE := kernel/yu/msm8937
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
